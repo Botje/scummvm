bb5927af38ae63a7e047d18608df28bc30beff6d
Dries Harnie <dries@harnie.be>
2025-12-19 19:49:02 +0100

    KQ8: Implement helpers for reading from INI files

--- engines/kq8/kq_file.h
+++ engines/kq8/kq_file.h
@@ -39,6 +39,63 @@ private:
 	SectionList _sections;
 };
 
+namespace INIHelpers {
+
+template<class T>
+T extractFromValue(const Common::String &);
+
+template<>
+inline Common::String extractFromValue(const Common::String &s) {
+	return s;
+}
+
+template<>
+inline float extractFromValue<float>(const Common::String &s) {
+	return strtof(s.c_str(), nullptr);
+}
+
+template<>
+inline uint8 extractFromValue<uint8>(const Common::String &s) {
+	return strtol(s.c_str(), nullptr, 10);
+}
+
+template<>
+inline uint16 extractFromValue<uint16>(const Common::String &s) {
+	return strtol(s.c_str(), nullptr, 10);
+}
+
+template<>
+inline int extractFromValue<int>(const Common::String &s) {
+	return strtol(s.c_str(), nullptr, 10);
+}
+
+template<>
+inline long extractFromValue<long>(const Common::String &s) {
+	return strtol(s.c_str(), nullptr, 10);
+}
+
+template<class T>
+T get(const KQFile::Section &sec, const Common::String &key, const T &def = T{}) {
+	auto *kv = sec.getKey(key);
+	if (!kv)
+		return def;
+	return extractFromValue<T>(kv->value);
+}
+
+template<class T>
+Common::Array<T> getArray(const KQFile::Section &sec, const Common::String &countKey, const Common::String &prefix) {
+	auto count = get<int>(sec, countKey);
+	Common::Array<T> array;
+	array.reserve(count);
+	for (int i = 0; i < count; i++) {
+		auto key = Common::String::format("%s%d", prefix.c_str(), i);
+		array.push_back(get<T>(sec, key));
+	}
+	return array;
+}
+
+} // namespace INIHelpers
+
 } // namespace Kq8
 
 #endif // KQ_FILE_H
